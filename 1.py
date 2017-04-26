6634300705c5d951775eee84f38c09446a8702c4
gjq246 <30488907@qq.com>
2017-04-26 23:39:21 +0800

    基础语法实验

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print "Hello, World!"; 
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+import support
+
+# 现在可以调用模块里包含的函数了
+support.print_func("Runoob");
+
+# 定义函数
+def printme( str ):
+   "打印任何传入的字符串"
+   print str;
+   return;
+ 
+# 调用函数
+printme("我要调用用户自定义函数!");
+printme("再次调用同一函数");
---
+++ support.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+def print_func( par ):
+   "模块函数"
+   print "Hello : ",par;
+   return
